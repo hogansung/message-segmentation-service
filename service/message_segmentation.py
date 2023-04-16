5f366ea5222cdbb809b9ebfa076ec84c27c1f9d4
hogansung <wrangle1005@gmail.com>
2023-04-16 00:58:47 -0700

    Add unicode support

--- service/message_segmentation.py
+++ service/message_segmentation.py
@@ -1,11 +1,15 @@
+import sys
 from typing import List, Union, Dict
-
 from flask import request, flash, Flask
 
-from message_segmentor import MessageSegmentor
+# from message_segmentor_v0 import MessageSegmentorV0
+from message_segmentor_v1 import MessageSegmentorV1
 
 api = Flask(__name__)
-message_segmentor = MessageSegmentor()
+# message_segmentor = MessageSegmentorV0(b_overwrite_db=False)
+message_segmentor = MessageSegmentorV1(b_overwrite_db=False)
+
+sys.setrecursionlimit(10000)
 
 
 @api.route("/query", methods=["POST"])
@@ -17,6 +21,12 @@ def query() -> Dict[str, List[Dict[str, Union[str, float]]]]:
     if "debug_mode" not in json:
         flash("`debug_mode` is required.")
 
+    # # To make escaped message normal message. For example r"\u1234" should be regarded as "\u1234".
+    # message = (
+    #     request.json["message"]
+    #     .encode("utf-8", "surrogateescape")
+    #     .decode("unicode-escape")
+    # )
     message = request.json["message"]
     debug_mode = request.json["debug_mode"]
 
